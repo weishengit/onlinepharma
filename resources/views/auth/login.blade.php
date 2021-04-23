461500962b1948a7b91f180b5f8f6a8c19693ca0
weishengit <weishenemail@gmail.com>
2021-04-23 23:34:52 +0800

    added error msg to login, made name unique

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,31 @@
         <div class="w-full h-100">
             <h1 class="mt-12 text-2xl font-semibold text-black tracking-ringtighter sm:text-3xl title-font">Log in to your
                 account</h1>
+            {{-- Message --}}
+            @if (session()->has('message'))
+            <div class="p-10 flex flex-col space-y-3">
+                <div class="bg-blue-100 p-5 w-full sm:w-1/2 border-l-4 border-blue-500">
+                    <div class="flex space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            class="flex-none fill-current text-blue-500 h-4 w-4">
+                            <path
+                                d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z" />
+                            </svg>
+                        <div class="flex-1 leading-tight text-sm text-blue-700">{{ session()->get('message') }}</div>
+                    </div>
+                </div>
+            </div>
+            @endif
+            {{-- Error --}}
+            @if ($errors->any())
+            <div class="mt-10 pl-2 text-red-600">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>* {{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
             <form class="mt-6" action="{{ route('login') }}" method="POST">
                 @csrf
                 <div>
@@ -25,7 +50,7 @@
                 </div>
                 <div class="mt-4">
                     <label class="block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">Password</label>
-                    <input type="password" name="password" id="" placeholder="Your Password" minlength="6"
+                    <input type="password" name="password" id="" placeholder="Your Password"
                         class="w-full px-4 py-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 "
                         required>
                 </div>
