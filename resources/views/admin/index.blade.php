7c98cbcc93c3aa6f54c315401ea9ab5678b7b227
Wei Shen <weishenemail@gmail.com>
2021-05-18 20:20:57 +0800

    fixed complete order icon

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -163,7 +163,7 @@
                 <div class="bg-white border rounded shadow p-2">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-red-600"><i class="fas fa-inbox fa-2x fa-fw fa-inverse"></i></div>
+                            <div class="rounded p-3 bg-blue-600"><i class="fas fa-check fa-2x fa-fw fa-inverse"></i></div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
                             <h5 class="font-bold uppercase text-gray-500">Confirm Completion</h5>
@@ -180,49 +180,6 @@
         <hr class="border-b-2 border-gray-400 my-8 mx-4">
 
         <div class="flex flex-row flex-wrap flex-grow mt-2">
-
-            {{-- <div class="w-full md:w-1/2 p-3">
-                <!--Graph Card-->
-                <div class="bg-white border rounded shadow">
-                    <div class="border-b p-3">
-                        <h5 class="font-bold uppercase text-gray-600">Graph</h5>
-                    </div>
-                    <div class="p-5">
-                        <canvas id="chartjs-7" class="chartjs" width="undefined" height="undefined"></canvas>
-                        <script>
-                        new Chart(document.getElementById("chartjs-7"), {
-                            "type": "bar",
-                            "data": {
-                                "labels": ["January", "February", "March", "April"],
-                                "datasets": [{
-                                    "label": "Page Impressions",
-                                    "data": [10, 20, 30, 40],
-                                    "borderColor": "rgb(255, 99, 132)",
-                                    "backgroundColor": "rgba(255, 99, 132, 0.2)"
-                                }, {
-                                    "label": "Adsense Clicks",
-                                    "data": [5, 15, 10, 30],
-                                    "type": "line",
-                                    "fill": false,
-                                    "borderColor": "rgb(54, 162, 235)"
-                                }]
-                            },
-                            "options": {
-                                "scales": {
-                                    "yAxes": [{
-                                        "ticks": {
-                                            "beginAtZero": true
-                                        }
-                                    }]
-                                }
-                            }
-                        });
-                        </script>
-                    </div>
-                </div>
-                <!--/Graph Card-->
-            </div> --}}
-
             <div class="w-full md:w-full p-3">
                 <!--Graph Card-->
                 <div class="bg-white border rounded shadow">
@@ -287,8 +244,8 @@
                             </script>
                         </div>
                     </div>
-                    <!--/Graph Card-->
-                </div>
+
+            </div>
 
             <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                 <!--Graph Card-->
